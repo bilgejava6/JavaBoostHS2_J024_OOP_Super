d7683dc2f819efc7064470d70695e542df889659
bilgejava6 <bilgejava6@gmail.com>
2024-11-17 17:01:26 +0300

    Ödev

---
+++ src/main/java/com/muhammet/odev/RunnerOdev.java
@@ -0,0 +1,16 @@
+package com.muhammet.odev;
+
+public class RunnerOdev {
+    public static void main(String[] args) {
+        /**
+         * --------> Urun
+         * ---- Bilgisayar
+         * ---- Mobilya
+         * ---- BeyazEşya
+         * ---- Giyim
+         * ---------------
+         * default olacak parametreleri siz belirliyorsunuz. bir ürün oluştuğunda neler
+         * atanmış olmalı? default ve parametreli constructor larını yazınız.
+         */
+    }
+}
